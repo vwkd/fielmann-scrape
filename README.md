531cb43a5e480cb77a4451d779171a546a668117
vwkd <33468089+vwkd@users.noreply.github.com>
2024-04-16 19:33:43 +0200

    parse: add

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Scrape products from Fielmann
 
 ## Features
 
-- fetch products
+- parse products
 - download images
 - render images with face
 
@@ -23,10 +23,18 @@ Scrape products from Fielmann
 - open https://www.fielmann.de/brillen/ in a browser, create the desired filters, and show results
 - note: recommends to run script multiple times for multiple specific filters to get multiple separate ouput directories, rather than single time for single unspecific filter to get single output directory
 - copy the filters from the URL into `.env`
-- fetch products
+- parse products
 
 ```sh
 deno task products
+```
+
+- note: can browse with Nushell
+- note: beware of empty cells, throws error if row has empty cell for selected column
+- note: price is for rim only, needs to add price for lenses (e.g. 17€ for basic lenses), website shows price with basic lenses
+
+```sh
+open out/productsParsed.json | select ean brand modelName frameColor shape rimType bridgeType hingeType headWidth lensWidth bridgeWidth templeLength price
 ```
 
 - download images
---
+++ src/features/parse.ts
@@ -0,0 +1,125 @@
+import { Attribute, AttributeMultiple } from "../types/products.ts";
+import { products } from "./products.ts";
+import { join } from "@std/path/join";
+
+const OUTPUT_DIRNAME = "out";
+const PRODUCTS_FILEPATH = join(OUTPUT_DIRNAME, "productsParsed.json");
+
+interface Attributes {
+  [name: string]: Attribute | AttributeMultiple;
+}
+
+/**
+ * Get attribute names and values
+ *
+ * - extracts `label` property, since nicely formatted value
+ * - except if `ean`, extracts `value` property
+ * - if multiple, concatenates with comma
+ *
+ * @param attributes object of attributes with name and nested structure
+ * @returns object of attributes with name and value
+ */
+function getAttributes(attributes: Attributes) {
+  const res = {};
+
+  for (const [name, attribute] of Object.entries(attributes)) {
+    if (attribute.multiSelect === true) {
+      res[name] = attribute.values.map((v) => v.label).join(", ");
+    } else {
+      // note: exception for `ean` attribute
+      if (name === "ean") {
+        res[name] = attribute.values.value;
+      } else {
+        res[name] = attribute.values.label;
+      }
+    }
+  }
+
+  return res;
+}
+
+console.info(`Parsing products...`);
+
+const productsParsed = [];
+
+for (const product of products) {
+  // id
+  // -
+  // isActive
+  // -
+  // isSoldOut
+  // -
+  // isNew
+  // -
+  // createdAt
+  // -
+  // updatedAt
+  // -
+  // indexedAt
+  // -
+  // firstLiveAt
+  // -
+  // masterKey
+  // -
+  // referenceKey
+  // -
+
+  // attributes
+  const attributes = getAttributes(product.attributes);
+
+  // advancedAttributes
+  // -
+  // images
+  // -
+
+  // variants
+  for (const variant of product.variants) {
+    // id
+    // -
+    // referenceKey
+    // -
+
+    // attributes
+    const variantAttributes = getAttributes(variant.attributes);
+
+    // advancedAttributes
+    // -
+    // firstLiveAt
+    // -
+    // createdAt
+    // -
+    // updatedAt
+    // -
+    // stock
+    // -
+
+    // price
+    const price = variant.price.withTax / 100;
+
+    // lowestPriorPrice
+    // -
+    // // note
+    // -
+    // customData
+    // -
+
+    productsParsed.push({
+      ...attributes,
+      ...variantAttributes,
+      price,
+    });
+  }
+
+  // priceRange
+  // -
+  // lowestPriorPrice
+  // -
+  // siblings
+  // -
+  // categories
+  // -
+  // customData
+  // -
+}
+
+Deno.writeTextFile(PRODUCTS_FILEPATH, JSON.stringify(productsParsed));
